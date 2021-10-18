0d102569afb3ec2cf19e4a73de2f9d065915e3c9
HipsterSantos <santoscampos269@gmail.com>
2021-10-18 12:20:53 +0100

    RecursiveComponent

--- src/App.js
+++ src/App.js
@@ -6,17 +6,28 @@ const hasChildren = items && items.length;
 return(
   <>
   {name}
-  {hasChildren && items.map((item.index)=>(
-    
-  ))}
+  {hasChildren && items.map((item,index)=>(
+    <RecursiveComponent {...item}/>
+  ))
+  }
   </>
 )
 }
 export default function App() {
+  const data = {
+    name: 'Level 1',
+    items: [{
+      name: 'Level 2',
+      items: [{
+        name: 'Level 3'
+      }]
+    }]
+  };
+  
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      
+      <RecursiveComponent {...data}/>
     </div>
   );
 }
